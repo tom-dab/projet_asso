851591e34364ee44c37ec96f46c4813d970125c8
tom <tom.dabrowski@ecoles-epsi.net>
2025-10-23 14:11:41 +0200

    Ajout d'une table statut

---
+++ src/main/java/epsi/b3/projet_asso/entity/Statut.java
@@ -0,0 +1,23 @@
+package epsi.b3.projet_asso.entity;
+
+public class Statut {
+    private Long id;
+    private String type;
+
+    // Getters et setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
